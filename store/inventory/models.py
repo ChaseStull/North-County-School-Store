8ce8766b25acfa40e8b0276a66812dc5206e89f3
Chase <chaseastull@gmail.com>
2017-04-26 12:34:51 -0400

    Improving individual item views

--- store/inventory/models.py
+++ store/inventory/models.py
@@ -5,7 +5,6 @@ class Item(models.Model):
     item_name = models.CharField(max_length=25)
     description = models.TextField('Description')
     stock = models.IntegerField('Items remaining')
-    image = models.URLField('Image url')
 
     def __str__(self):
         return self.item_name
